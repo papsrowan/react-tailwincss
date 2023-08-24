e4f6e514e557592a21b555f9e500774fad38b6cd
papsrowan <papsrowan@gmail.com>
2023-08-24 15:20:03 +0200

    initiation à tailwincss

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-[100vh] bg-slate-900">
+      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div class="shrink-0">
+          <img class="h-12 w-12" src={logo} alt="ChitChat Logo"></img>
+        </div>
+        <div>
+          <div class="text-xl font-medium text-black">ChitChat</div>
+          <p class="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
+
+      <div class="bg-white max-w-sm mx-auto rounded-xl p-6 flex items-center space-x-4">
+        <div class="shrink-0">
+          <img class="h-12 w-12" src={logo} alt="ChitChat Logo"></img>
+        </div>
+        <div>
+          <div class="text-xl text-black font-medium">ChitChat</div>
+          <p class="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
     </div>
+
+    
   );
 }
 
